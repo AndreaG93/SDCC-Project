42e88a27dc57bbe1a10ae57a814e5bb2356496ae
Andrea Graziani <andrea.graziani93@outlook.it>
2019-09-09 18:55:19 +0200

    Other changes :( :(

--- script/AmazonEC2-CloudSystemConfiguration.sh
+++ script/AmazonEC2-CloudSystemConfiguration.sh
@@ -1,15 +1,29 @@
-# sudo apt install awscli (on Ubuntu)
-# chmod 077 graziani.pem
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Amazon EC2 instances configuration
+#
+# sudo apt install awscli jq (on Ubuntu)
+# chmod u=xrw,g=,o= ./graziani.pem
+#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Zookeeper nodes configuration on Amazon EC2
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# System's internet addesses...
+PRIMARY_INSTENCES_PUBLIC_IP=()
+PRIMARY_INSTENCES_PRIVATE_IP=()
+
+ZOOKEEPER_INSTENCES_PRIVATE_IP=()
+ZOOKEEPER_INSTENCES_PUBLIC_IP=()
+
+WORKER_GROUP_0_INSTENCES_PRIVATE_IP=()
+WORKER_GROUP_0_INSTENCES_PUBLIC_IP=()
+
+WORKER_GROUP_1_INSTENCES_PRIVATE_IP=()
+WORKER_GROUP_1_INSTENCES_PUBLIC_IP=()
+
+# ======================================================================================================================
+# Zookeeper Servers
+# ======================================================================================================================
 
 # 1 - Information retrival...
-# ==================================================================================================================== #
-ZOOKEEPER_SERVER_PRIVATE_IP=()
-ZOOKEEPER_SERVER_PUBLIC_IP=()
-
 for i in 1 2 3
 do
     EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=ZookeeperServer" "Name=tag:ID,Values=$i")
@@ -17,115 +31,190 @@ do
     OUTPUT1=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress')
     OUTPUT2=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
 
-    ZOOKEEPER_SERVER_PRIVATE_IP+=("$OUTPUT1")
-    ZOOKEEPER_SERVER_PUBLIC_IP+=("$OUTPUT2")
+    ZOOKEEPER_INSTENCES_PRIVATE_IP+=("$OUTPUT1")
+    ZOOKEEPER_INSTENCES_PUBLIC_IP+=("$OUTPUT2")
 done
 
-# 2 - Configuration
-# ==================================================================================================================== #
+# 3 - Start
+for i in 1 2 3
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=ZookeeperServer" "Name=tag:ID,Values=$i")
+    IP=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+    ssh -i "graziani.pem" ubuntu@$IP "sudo /usr/local/zookeeper/bin/zkServer.sh start"
+done
 
+# 4 - Status
+for i in 1 2 3
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=ZookeeperServer" "Name=tag:ID,Values=$i")
+    IP=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+    ssh -i "graziani.pem" ubuntu@$IP "sudo /usr/local/zookeeper/bin/zkServer.sh status"
+done
+
+# ======================================================================================================================
+# Primary Servers
+# ======================================================================================================================
+
+# Information retrival...
+for i in 1 2 3
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=PrimaryServer" "Name=tag:ID,Values=$i")
+    OUTPUT=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+
+    PRIMARY_INSTENCES_PUBLIC_IP+=("$OUTPUT")
+done
+
+# Configuration
 index=0
-for i in "${ZOOKEEPER_SERVER_PUBLIC_IP[@]}"
+for i in "${PRIMARY_INSTENCES_PUBLIC_IP[@]}"
 do
     ((index++))
     ssh -i "graziani.pem" ubuntu@$i "
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y default-jre
-
-sudo wget https://www-us.apache.org/dist/zookeeper/zookeeper-3.5.5/apache-zookeeper-3.5.5-bin.tar.gz
-
-sudo tar -xzf  apache-zookeeper-3.5.5-bin.tar.gz
-sudo mv apache-zookeeper-3.5.5-bin /usr/local/zookeeper
-
-echo 'tickTime=2000
-initLimit=10
-syncLimit=5
-dataDir=/var/lib/zookeeper
-clientPort=2181
-server.1=${ZOOKEEPER_SERVER_PRIVATE_IP[0]}:2888:3888
-server.2=${ZOOKEEPER_SERVER_PRIVATE_IP[1]}:2888:3888
-server.3=${ZOOKEEPER_SERVER_PRIVATE_IP[2]}:2888:3888' | sudo tee /usr/local/zookeeper/conf/zoo.cfg
-
-echo $index | sudo tee /var/lib/zookeeper/myid
-    "
-done
-
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Primary nodes configuration on Amazon EC2
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-# Information retrival...
-# ==================================================================================================================== #
-
-PRIMARY_SERVERS_PUBLIC_IP=()
-
-for i in 1 2 3
-do
-    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=PrimaryServer" "Name=tag:ID,Values=$i")
-
-    OUTPUT1=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
-
-    PRIMARY_SERVERS_PUBLIC_IP+=("$OUTPUT1")
-done
-
-# Configuration
-# ==================================================================================================================== #
-
-index=0
-for i in "${PRIMARY_SERVERS_PUBLIC_IP[@]}"
-do
-    ((index++))
-    ssh -i "graziani-01.pem" ubuntu@$i "
-
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update && sudo apt install -y golang jq
 
 go get -u github.com/aws/aws-sdk-go/service/s3/...
 go get -u github.com/aws/aws-sdk-go/aws/...
 go get -u github.com/samuel/go-zookeeper/zk
 go get -u github.com/Sirupsen/logrus
 
+rm -R ./go/src/SDCC-Project
 git -C ./go/src clone https://github.com/AndreaG93/SDCC-Project
 
-jq -n "{ZookeeperServersPrivateIPs: [\"${ZOOKEEPER_SERVER_PRIVATE_IP[0]}\",\"${ZOOKEEPER_SERVER_PRIVATE_IP[1]}\",\"${ZOOKEEPER_SERVER_PRIVATE_IP[2]}\"], NodeID: $index, NodeGroupID: 0, NodeClass: \"Primary\"}" > ./go/src/SDCC-Project/main/conf.json"
+jq -n '{ZookeeperServersPrivateIPs: [\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[0]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[1]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[2]}\"], NodeID: $index, NodeGroupID: 0, NodeClass: \"Primary\"}' > ./conf.json"
 done
 
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Worker nodes configuration on Amazon EC2
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# 3 - Start
+for i in 1 2 3
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=PrimaryServer" "Name=tag:ID,Values=$i")
+    IP=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+
+    ssh -i "graziani.pem" ubuntu@$IP "
+    go run ./go/src/SDCC-Project/aftmapreduce/main/entrypoint.go &"
+done
+
+# ======================================================================================================================
+# Worker Servers GROUP 0
+# ======================================================================================================================
 
 # Information retrival...
-# ==================================================================================================================== #
-
-WORKER_SERVERS_PUBLIC_IP()
-
-for workerGroupId in 0 1 2 3 4 5
-for workerGroupId in 0 1
+for i in 0 1 2
 do
-    echo $groupid
-    echo $workerGroupId
-
-    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=Worker" "Name=tag:ID,Values=$id","Name=tag:ID-Group,Values=$groupid")
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=Worker" "Name=tag:ID,Values=$id","Name=tag:ID-Group,Values=0")
     OUTPUT1=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
-    WORKER_SERVERS_PUBLIC_IP+=("$OUTPUT1")
-done
+    WORKER_GROUP_0_INSTENCES_PUBLIC_IP+=("$OUTPUT1")
 done
 
 # Configuration
-# ==================================================================================================================== #
-
 index=0
-for i in "${WORKER_SERVERS_PUBLIC_IP[@]}"
+for i in "${WORKER_GROUP_0_INSTENCES_PUBLIC_IP[@]}"
 do
-    ((index++))
-    ssh -i "graziani-01.pem" ubuntu@$i "
+    ssh -i "graziani.pem" ubuntu@$i "
 
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update && sudo apt install -y golang jq
 
 go get -u github.com/aws/aws-sdk-go/service/s3/...
 go get -u github.com/aws/aws-sdk-go/aws/...
-go get -u github.com/samuel/go-zookeeper/z
+go get -u github.com/samuel/go-zookeeper/zk
+go get -u github.com/Sirupsen/logrus
 
+rm -R ./go/src/SDCC-Project
 git -C ./go/src clone https://github.com/AndreaG93/SDCC-Project
 
-jq -n "{ZookeeperServersPrivateIPs: [\"${ZOOKEEPER_SERVER_PRIVATE_IP[0]}\",\"${ZOOKEEPER_SERVER_PRIVATE_IP[1]}\",\"${ZOOKEEPER_SERVER_PRIVATE_IP[2]}\"], NodeID: $index, NodeClass: \"Worker\"}" > ./go/src/SDCC-Project/main/conf.json"
+jq -n '{ZookeeperServersPrivateIPs: [\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[0]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[1]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[2]}\"], NodeID: $index, NodeGroupID: 0, NodeClass: \"Worker\"}' > ./conf.json"
+  ((index++))
 done
+
+# 3 - Start
+for i in 0 1 2
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=Worker" "Name=tag:ID,Values=$id","Name=tag:ID-Group,Values=0")
+    IP=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+
+    ssh -i "graziani.pem" ubuntu@$IP "
+    go run ./go/src/SDCC-Project/aftmapreduce/main/entrypoint.go &"
+done
+
+# ======================================================================================================================
+# Worker Servers GROUP 1
+# ======================================================================================================================
+
+# Information retrival...
+for i in 3 4 5
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=Worker" "Name=tag:ID,Values=$id","Name=tag:ID-Group,Values=1")
+    OUTPUT1=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+    WORKER_GROUP_0_INSTENCES_PUBLIC_IP+=("$OUTPUT1")
+done
+
+# Configuration
+index=3
+for i in "${WORKER_GROUP_0_INSTENCES_PUBLIC_IP[@]}"
+do
+    ssh -i "graziani.pem" ubuntu@$i "
+
+sudo apt update && sudo apt install -y golang jq
+
+go get -u github.com/aws/aws-sdk-go/service/s3/...
+go get -u github.com/aws/aws-sdk-go/aws/...
+go get -u github.com/samuel/go-zookeeper/zk
+go get -u github.com/Sirupsen/logrus
+
+rm -R ./go/src/SDCC-Project
+git -C ./go/src clone https://github.com/AndreaG93/SDCC-Project
+
+jq -n '{ZookeeperServersPrivateIPs: [\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[0]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[1]}\",\"${ZOOKEEPER_INSTENCES_PRIVATE_IP[2]}\"], NodeID: $index, NodeGroupID: 0, NodeClass: \"Worker\"}' > ./conf.json"
+    ((index++))
+done
+
+# 3 - Start
+for i in 3 4 5
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=Worker" "Name=tag:ID,Values=$id","Name=tag:ID-Group,Values=1")
+    IP=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+
+    ssh -i "graziani.pem" ubuntu@$IP "
+    go run ./go/src/SDCC-Project/aftmapreduce/main/entrypoint.go &"
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script/AmazonEC2-ZookeeperConfiguration.sh
@@ -0,0 +1,41 @@
+ZOOKEEPER_INSTENCES_PRIVATE_IP=()
+ZOOKEEPER_INSTENCES_PUBLIC_IP=()
+
+for i in 1 2 3
+do
+    EC2_OUTPUT=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Role,Values=ZookeeperServer" "Name=tag:ID,Values=$i")
+
+    OUTPUT1=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress')
+    OUTPUT2=$(echo $EC2_OUTPUT | jq -r '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp')
+
+    ZOOKEEPER_INSTENCES_PRIVATE_IP+=("$OUTPUT1")
+    ZOOKEEPER_INSTENCES_PUBLIC_IP+=("$OUTPUT2")
+done
+
+# 2 - Configuration
+index=0
+for i in "${ZOOKEEPER_INSTENCES_PUBLIC_IP[@]}"
+do
+
+((index++))
+ssh -i "graziani.pem" ubuntu@$i "
+
+sudo apt update && sudo apt install -y default-jre
+
+sudo wget https://www-us.apache.org/dist/zookeeper/zookeeper-3.5.5/apache-zookeeper-3.5.5-bin.tar.gz
+
+sudo tar -xzf  apache-zookeeper-3.5.5-bin.tar.gz
+sudo mv apache-zookeeper-3.5.5-bin /usr/local/zookeeper
+
+echo 'tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/var/lib/zookeeper
+clientPort=2181
+server.1=${ZOOKEEPER_INSTENCES_PRIVATE_IP[0]}:2888:3888
+server.2=${ZOOKEEPER_INSTENCES_PRIVATE_IP[1]}:2888:3888
+server.3=${ZOOKEEPER_INSTENCES_PRIVATE_IP[2]}:2888:3888' | sudo tee /usr/local/zookeeper/conf/zoo.cfg
+
+echo $index | sudo tee -a /var/lib/zookeeper/myid
+"
+done
