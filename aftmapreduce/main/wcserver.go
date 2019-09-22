e95e3ce83798d87f302924d97da346cfc2e59d17
Andrea Graziani <andrea.graziani93@outlook.it>
2019-09-22 17:51:32 +0200

    Final COMMIT

--- aftmapreduce/main/wcserver.go
+++ aftmapreduce/main/wcserver.go
@@ -6,37 +6,45 @@ import (
 	"SDCC-Project/aftmapreduce/process/worker"
 	"SDCC-Project/aftmapreduce/utility"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
 
+	var nodePublicIP string
+
 	configuration := new(utility.NodeConfiguration)
-	if err := configuration.Load("./conf.json"); err != nil {
+	if err := configuration.Load("conf.json"); err != nil {
 		panic(err)
 	}
 
-	command := exec.Command("curl", "http://169.254.169.254/latest/meta-data/public-ipv4")
+	if len(os.Args) == 1 {
 
-	if commandOutput, err := command.Output(); err != nil {
-		panic(err)
-	} else {
-
-		nodeID := configuration.NodeID
-		nodeClass := configuration.NodeClass
-		nodeGroupId := configuration.NodeGroupID
-		zookeeperServers := configuration.ZookeeperServersPrivateIPs
-		nodePublicIP := string(commandOutput)
-
-		utility.CheckError(process.Initialize(nodeID, nodeGroupId, nodeClass, nodePublicIP, zookeeperServers))
-
-		if nodeClass == "Primary" {
-			fmt.Printf("Start as Primary -- Node %d", nodeID)
-			primary.StartWork()
-
-		} else if nodeClass == "Worker" {
-			fmt.Printf("Start as Worker -- Node %d, Group %d", nodeID, nodeGroupId)
-			worker.StartWork()
+		command := exec.Command("curl", "http://169.254.169.254/latest/meta-data/public-ipv4")
+		if commandOutput, err := command.Output(); err != nil {
+			panic(err)
+		} else {
+			nodePublicIP = string(commandOutput)
 		}
+
+	} else {
+		nodePublicIP = "localhost"
+	}
+
+	nodeID := configuration.NodeID
+	nodeClass := configuration.NodeClass
+	nodeGroupId := configuration.NodeGroupID
+	zookeeperServers := configuration.ZookeeperServersPrivateIPs
+
+	utility.CheckError(process.Initialize(nodeID, nodeGroupId, nodeClass, nodePublicIP, zookeeperServers))
+
+	if nodeClass == "Primary" {
+		fmt.Printf("Start as Primary -- Node %d", nodeID)
+		primary.StartWork()
+
+	} else if nodeClass == "Worker" {
+		fmt.Printf("Start as Worker -- Node %d, Group %d", nodeID, nodeGroupId)
+		worker.StartWork()
 	}
 }
